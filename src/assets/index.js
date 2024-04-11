Vetrivel-VP/react-starter-with-tailwindcss-for-portfolio

Changes : Project Starter Initiated

src/assets/index.js
@@ -1,0 +1,16 @@
+export { default as Hero } from "./img/hero.png";
+export { default as Leaf1 } from "./img/Leaf.png";
+export { default as Leaf2 } from "./img/leaf2.png";
+export { default as about } from "./img/about.webp";
+
+// projects
+
+export { default as RestaurantClone } from "./img/Project/thumb_restaurant_app_mar22.png";
+export { default as CodePenClone } from "./img/Project/Thumb-codepen-clone.png";
+export { default as OpenAI } from "./img/Project/Thumb-openai-withreactjs.png";
+export { default as ChatApp } from "./img/Project/Thumb-expo-chat-app.png";
+export { default as ImageSharing } from "./img/Project/Thumb-image-sharing-app-react-native-mar-23-updated.png";
+export { default as PixabayClone } from "./img/Project/Thumb-pixabay-clone-v2–1.png";
+export { default as FreshJuiceUI } from "./img/Project/thumb-fresh0juics-tweenmax.jpg";
+export { default as SocialMedia } from "./img/Project/thumb-fullstack-socialmedia-app-react-sanity.png";
+export { default as PortfolioFirebase } from "./img/Project/thumb-portfolio-firebase.jpeg";

siyaajoshi/siyaajoshi.github.io

src/assets/index.js
@@ -1,0 +1,16 @@
+export { default as Siya } from "./img/Siya.png";
+export { default as Leaf1 } from "./img/Leaf.png";
+export { default as Leaf2 } from "./img/leaf2.png";
+export { default as about } from "./img/about.webp";
+
+// projects
+
+export { default as RestaurantClone } from "./img/Project/thumb_restaurant_app_mar22.png";
+export { default as CodePenClone } from "./img/Project/Thumb-codepen-clone.png";
+export { default as OpenAI } from "./img/Project/Thumb-openai-withreactjs.png";
+export { default as ChatApp } from "./img/Project/Thumb-expo-chat-app.png";
+export { default as ImageSharing } from "./img/Project/Thumb-image-sharing-app-react-native-mar-23-updated.png";
+export { default as PixabayClone } from "./img/Project/Thumb-pixabay-clone-v2–1.png";
+export { default as FreshJuiceUI } from "./img/Project/thumb-fresh0juics-tweenmax.jpg";
+export { default as SocialMedia } from "./img/Project/thumb-fullstack-socialmedia-app-react-sanity.png";
+export { default as PortfolioFirebase } from "./img/Project/thumb-portfolio-firebase.jpeg";